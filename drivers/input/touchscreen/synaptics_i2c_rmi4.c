621bd1afebcff5be837c5f55c37d00e01a80924c
zeroblade1984 <zeroblade1984@gmail.com>
2015-02-16 22:52:22 +0800

    Input:Small fix for Synaptics TS panel

--- drivers/input/touchscreen/synaptics_i2c_rmi4.c
+++ drivers/input/touchscreen/synaptics_i2c_rmi4.c
@@ -3675,13 +3675,20 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 		rmi4_data && rmi4_data->i2c_client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK)
-			synaptics_rmi4_resume(&(rmi4_data->input_dev->dev));
-		else if (*blank == FB_BLANK_POWERDOWN)
-			synaptics_rmi4_suspend(&(rmi4_data->input_dev->dev));
+		switch (*blank) {
+			case FB_BLANK_UNBLANK:
+				synaptics_rmi4_resume(&(rmi4_data->input_dev->dev));
+				break;
+			case FB_BLANK_POWERDOWN:
+			case FB_BLANK_HSYNC_SUSPEND:
+			case FB_BLANK_VSYNC_SUSPEND:
+			case FB_BLANK_NORMAL:
+				synaptics_rmi4_suspend(&(rmi4_data->input_dev->dev));
+				break;
+		}
 	}
 
-	return 0;
+	return NOTIFY_OK;
 }
 #elif defined(CONFIG_HAS_EARLYSUSPEND)
  /**
